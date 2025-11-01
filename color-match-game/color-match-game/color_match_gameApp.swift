477b765e0affb87dfeb52f4e55311455092b4972
swoichha <swoichhaa@gmail.com>
2025-11-01 17:49:44 -0400

    feat: swiftUI project initial setup

---
+++ color-match-game/color-match-game/color_match_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  color_match_gameApp.swift
+//  color-match-game
+//
+//  Created by swoichha adhikari on 01/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct color_match_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ color-match-gameTests/color_match_gameTests.swift
@@ -0,0 +1,17 @@
+//
+//  color_match_gameTests.swift
+//  color-match-gameTests
+//
+//  Created by swoichha adhikari on 01/11/2025.
+//
+
+import Testing
+@testable import color_match_game
+
+struct color_match_gameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
